f4d3892dfe96f821b8008891d5b02bfa60be6c16
giovanni-arduini <gionnibigudd@gmail.com>
2025-06-11 11:15:53 +0200

    ADD first index.php file

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Il mio primo PHP</title>
+  </head>
+  <body>
+    <h1>
+    Ciao mamma!
+    </h1>
+    
+    <!-- questo è uno squacio php -->
+    <?php 
+    // sono un commento!
+
+  // supervariabili globali
+    echo $
+
+    // echo serve per visualizzare codice in pagina
+    echo "E ciao mondo!";
+
+    // variabili
+    $name = "Gio";
+    echo "Ciao " . $name;
+
+    $eta = 34;
+    echo $eta;
+    ?>
+  </body>
+</html>
