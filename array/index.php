e8547cba0b0d9de2768aff74d8c1aa1405fa6f51
giovanni-arduini <gionnibigudd@gmail.com>
2025-10-02 12:34:09 +0200

    feat: add array examples

---
+++ array/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays</title>
+</head>
+<body>
+    
+    <h1>PHP Fundamentals</h1>
+
+    <?php
+    $person = [
+        "first_name" => "Gigi",
+        "last_name"=>"Lollo",
+        "hobby"=>"Soccer"
+    ];
+
+    echo $person["hobby"];
+
+    ?>
+
+</body>
+</html>
