78b771a90ecb58ffee89a42e5e47d489f6db8aa9
giovanni-arduini <gionnibigudd@gmail.com>
2025-10-04 15:40:58 +0200

    feat add cart page with session variables

---
+++ functions/cart.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+// var_dump($_SESSION);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include "./header.php";
+    ?>
+    <h1>Carrello</h1>
+    <h3>Nome utente: <?php  echo $_SESSION["username"]?></h3>
+</body>
+</html>
